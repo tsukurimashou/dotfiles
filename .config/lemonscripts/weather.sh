e7c71e204df23ca05d8e073ec0f154eac602f4f5
Leo Razbinski <l.razbinski@secondfloor.com>
2018-01-15 12:22:49 +0100

    fixed negative values for weather

--- .config/lemonscripts/weather.sh
+++ .config/lemonscripts/weather.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 Weather() {
-  AVG=$(cat $HOME/startpage/weather.json | jq -r '.day_1 .avgtemp_c')
   MIN=$(cat $HOME/startpage/weather.json | jq -r '.day_1 .mintemp_c')
   MAX=$(cat $HOME/startpage/weather.json | jq -r '.day_1 .maxtemp_c')
+  AVG=$(cat $HOME/startpage/weather.json | jq -r '.day_1 .avgtemp_c')
   TXT=$(cat $HOME/startpage/weather.json | jq -r '.day_1 .condition .text')
-  printf "YMIN-%.0f:" "$MIN"
-  printf "MAX-%.0f:" "$MAX"
-  printf "AVG-%.0f:" "$AVG"
-  printf "TXT-$TXT\n"
+  printf "YMIN#%.0f:" "$MIN"
+  printf "MAX#%.0f:" "$MAX"
+  printf "AVG#%.0f:" "$AVG"
+  printf "TXT#$TXT\n"
 }
 
 while true; do
