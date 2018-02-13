01a92c577a886a7719942dd4f19592dd8448e234
Leo Razbinski <l.razbinski@secondfloor.com>
2018-02-13 15:27:39 +0100

    updated lock script to use ffmpeg instead of imagemagick

--- bin/lock.sh
+++ bin/lock.sh
@@ -5,10 +5,9 @@ TMP="/tmp/scrot.png"
 
 scrot $TMP
 if [ -f $TMP ]; then
-	# blur
-	convert $TMP -blur 0x8 -define filter:sigma=2 $TMP
-	# add icon
-	convert $TMP $ICON -gravity center -composite -matte $TMP
+	# grab screenshot, blur and add icon in the middle
+  RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
+  ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $ICON -filter_complex "boxblur=10:2,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMP -loglevel quiet
 	# lock
 	i3lock -u -i $TMP
 	# rm temp file
